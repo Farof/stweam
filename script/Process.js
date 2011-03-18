147a80562adcb30d50abec3171f582cc8ac2162d
Farof <gfarof@gmail.com>
2011-03-18 19:04:53 +0100

    Revert "Proof of concept canvas mouse interaction"
    This reverts commit f2da4c8821283e25419548b544cc29e1be765fad.

--- script/Process.js
+++ script/Process.js
@@ -110,59 +110,21 @@
       return this;
     },
     
-    canvasStatus: {
-      overItem: null,
-      overPath: {},
-      mouseX: null,
-      mouseY: null
-    },
-    
     toWorkspaceElement: function () {
       var el, htmlEl, canvasEl, i, ln;
       if (!this.workspaceZone) {
         this.workspaceZone = el = new Element('div', {
           id: 'workspace',
-          process: this,
-          events: {
-            mousedown: function (e) {
-              this.process.handleWorkspaceMousedown.call(htmlEl, e);
-            },
-            
-            mouseover: function (e) {
-              var target = e.target;
-            },
-            
-            mousemove: function (e) {
-              var
-              target = e.target,
-              process = this.process,
-              pos = Element.pos(process.canvas),
-              item = target.classList.contains('workspace-item') ? target : Element.getParentByClass(target, 'workspace-item');
-              
-              if ((!process.canvasStatus.overItem && item) ||
-                  (process.canvasStatus.overItem && item && process.canvasStatus.overItem != item)) {
-                process.canvasStatus.overItem = item;
-              } else if (process.canvasStatus.overItem && !item) {
-                process.canvasStatus.overItem = null;
-              }
-              
-              process.canvasStatus.mouseX = e.clientX - pos.x + document.documentElement.scrollLeft;
-              process.canvasStatus.mouseY = e.clientY - pos.y + document.documentElement.scrollTop;
-              process.drawCanvas(e);
-            },
-            
-            click: function (e) {
-              var pos = Element.pos(canvasEl);
-              console.log('( ' + (e.clientX - pos.x) + ' ; ' + (e.clientY - pos.y) + ' )', e);
-            }
-          }
+          process: this
         });
         this.workspace = htmlEl = new Element('div', {
           id: 'workHtml',
-          process: this
+          process: this,
+          events: {
+            mousedown: this.handleWorkspaceMousedown
+          }
         });
         el.appendChild(htmlEl);
-        
         this.canvas = canvasEl = new Element('canvas', {
           id: 'workCanvas',
           height: '500',
@@ -269,84 +231,29 @@
       this.canvas.width = this.canvas.width;
     },
     
-    canvasConf: {
-      mouse: {
-        circle: {
-          radius: { normal: 5 },
-          borderWidth: { normal: 1 },
-          borderColor: { normal: '#3E3D40' }
-        }
-      },
-      itemPath: {
-        dot: {
-          radius: { normal: 3, overSource: 5 },
-          borderWidth: { normal: 1, overSource: 2 },
-          fillColor: { normal: '#3E3D40', overSource: '#FF0000' },
-          borderColor: { normal: '#3E3D40', get overSource() { return this.normal; } }
-        },
-        line: {
-          width: { normal: 2, over: 3 },
-          color: { normal: '#3E3D40', get over() { return this.normal; } },
-          shadowColor: { normal: '#3E3D40', over: '#FF0000' },
-          shadowBlur: { normal: 0, over: 2 }
-        },
-        arrow: {
-          width: { normal: 5 },
-          height: { normal: 7 },
-          color: { normal: '#3E3D40' }
-        }
-      }
-    },
-    
     drawCanvas: function () {
       this.clearCanvas();
       this.drawPaths();
-      this.drawMouse();
-      
-      if (this.canvasStatus.redraw === true) {
-        this.canvasStatus.redraw = false;
-        this.drawCanvas();
-        this.canvasStatus.redraw = null;
-      }
     },
     
-    setRedraw: function () {
-      this.canvasStatus.redraw = (this.canvasStatus.redraw !== false) ? true : false;
-    },
-    
-    drawMouse: function () {
-      var
-      ctx = this.ctx,
-      conf = this.canvasConf.mouse,
-      status = this.canvasStatus;
-      
-      ctx.beginPath();
-      ctx.arc(status.mouseX, status.mouseY, conf.circle.radius.normal, 0, 2 * Math.PI, false);
-      ctx.lineWidth = conf.circle.borderWidth.normal;
-      ctx.strokeStyle = conf.circle.borderColor.normal;
-      ctx.stroke();
-      ctx.closePath();
-    },
-    
-    drawPaths: function (e) {
+    drawPaths: function () {
       this.items.forEach(function (item) {
         if (item.input) {
-          this.drawPathBetween(item.input.workspaceElement, item.workspaceElement, e);
+          this.drawPathBetween(item.input.workspaceElement, item.workspaceElement);
         }
       }.bind(this));
     },
     
-    drawPathBetween: function (source, dest, e) {
+    drawPathBetween: function (source, dest) {
       var
       ctx = this.ctx,
       
-      // path conf
-      status = this.canvasStatus,
-      conf = this.canvasConf.itemPath,
-      overSource = status.overItem === source,
-      overPath = status.overPath && status.overPath.source === source && status.overPath.dest === dest,
+      dotRadius = 3,
+      dotBorderWidth = 1,
+      lineWidth = 2,
+      arrowWidth = 5,
+      arrowHeight = 7,
       
-      // positions infos
       sourceCenterX = source.offsetLeft + (source.scrollWidth / 2),
       sourceCenterY = source.offsetTop + (source.scrollHeight / 2),
       destCenterX = dest.offsetLeft + (dest.scrollWidth / 2),
@@ -357,7 +264,7 @@
       diffY = Math.abs(sourceCenterY - destCenterY),
       diffIsHeight = diffY > diffX,
 
-      // coordinates
+
       startX = source.offsetLeft + (diffIsHeight ? (source.scrollWidth / 2) : (sourceOnLeft ? source.scrollWidth : 0)),
       startY = source.offsetTop + (!diffIsHeight ? (source.scrollHeight / 2) : (sourceOnTop ? source.scrollHeight : 0)),
       startControlX = startX + (diffIsHeight ? 0 : (sourceOnLeft ? 50 : -50)),
@@ -366,19 +273,20 @@
       endY = dest.offsetTop + (!diffIsHeight ? (dest.scrollHeight / 2) : (!sourceOnTop ? dest.scrollHeight : 0)),
       endControlX = endX + (diffIsHeight ? 0 : (!sourceOnLeft ? 50 : -50)),
       endControlY = endY + (!diffIsHeight ? 0 : (!sourceOnTop ? 50 : -50)),
-      arrowStartX = endX + (diffIsHeight ? -conf.arrow.width.normal : (sourceOnLeft ? -conf.arrow.height.normal : conf.arrow.height.normal)),
-      arrowStartY = endY + (!diffIsHeight ? -conf.arrow.width.normal : (sourceOnTop ? -conf.arrow.height.normal : conf.arrow.height.normal)),
-      arrowEndX = endX + (diffIsHeight ? conf.arrow.width.normal : (sourceOnLeft ? -conf.arrow.height.normal : conf.arrow.height.normal)),
-      arrowEndY = endY + (!diffIsHeight ? conf.arrow.width.normal : (sourceOnTop ? -conf.arrow.height.normal : conf.arrow.height.normal));
+      arrowStartX = endX + (diffIsHeight ? -arrowWidth : (sourceOnLeft ? -arrowHeight : arrowHeight)),
+      arrowStartY = endY + (!diffIsHeight ? -arrowWidth : (sourceOnTop ? -arrowHeight : arrowHeight)),
+      arrowEndX = endX + (diffIsHeight ? arrowWidth : (sourceOnLeft ? -arrowHeight : arrowHeight)),
+      arrowEndY = endY + (!diffIsHeight ? arrowWidth : (sourceOnTop ? -arrowHeight : arrowHeight));
 
+      console.log('diffs: ', diffX, diffY, diffIsHeight);
 
       // start dot
       ctx.beginPath();
-      ctx.arc(startX, startY, conf.dot.radius[overSource ? 'overSource' : 'normal'], 0, 2 * Math.PI, false);
-      ctx.fillStyle = conf.dot.fillColor[overSource ? 'overSource' : 'normal'];
+      ctx.arc(startX, startY, dotRadius, 0, 2 * Math.PI, false);
+      ctx.fillStyle = '#3E3D40';
       ctx.fill();
-      ctx.lineWidth = conf.dot.borderWidth[overSource ? 'overSource' : 'normal'];
-      ctx.strokeStyle = conf.dot.borderColor[overSource ? 'overSource' : 'normal'];
+      ctx.lineWidth = dotBorderWidth;
+      ctx.strokeStyle = '#3E3D40';
       ctx.stroke();
       ctx.closePath();
 
@@ -386,25 +294,11 @@
       ctx.beginPath();
       ctx.moveTo(startX, startY);
       ctx.bezierCurveTo(startControlX, startControlY, endControlX, endControlY, endX, endY);
-      ctx.shadowColor = conf.line.shadowColor[overPath ? 'over' : 'normal'];
-      ctx.shadowBlur = conf.line.shadowBlur[overPath ? 'over' : 'normal'];
-      ctx.lineWidth = conf.line.width[overPath ? 'over' : 'normal'];
-      ctx.strokeStyle = conf.line.color[overPath ? 'over' : 'normal'];
+      ctx.lineWidth = lineWidth;
+      ctx.strokeStyle = '#3E3D40';
       ctx.lineCap = 'round';
       ctx.stroke();
       ctx.closePath();
-      // set over path flags
-      if (ctx.isPointInPath(status.mouseX, status.mouseY)) {
-        status.overPath.source = source;
-        status.overPath.dest = dest;
-        document.body.style.cursor = 'pointer';
-        this.setRedraw();
-      } else if (overPath) {
-        status.overPath.source = null;
-        status.overPath.dest = null;
-        document.body.style.cursor = 'auto';
-      }
-      
       
       // end arrow
       ctx.beginPath();
@@ -412,20 +306,8 @@
       ctx.lineTo(endX, endY);
       ctx.moveTo(endX, endY);
       ctx.lineTo(arrowEndX, arrowEndY);
-      ctx.strokeStyle = conf.arrow.color.normal;
       ctx.stroke();
       ctx.closePath();
-    },
-    
-    _drawCurve: function (startX, startY, endX, endY, options) {
-      var ctx = this.ctx, conf = options.conf;
-      
-      
-      if (!options.over && ctx.isPointInPath(status.mouseX, status.mouseY)) {
-        console.log('over');
-        options.over = true;
-        this._drawCurve.call(this, startX, startY, endX, endY, options);
-      }
     },
     
     itemUpdated: function (updateType, item) {
