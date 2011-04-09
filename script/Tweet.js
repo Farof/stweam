5947726cf881e1325f8860e1b3bb52a5255a4d68
Farof <gfarof@gmail.com>
2011-04-09 02:28:17 +0200

    regexp source

--- script/Tweet.js
+++ script/Tweet.js
@@ -82,6 +82,7 @@
       function (text, element) {
         var
           p = element.querySelector('.tweet'),
+          // regexp from John Gruber: http://daringfireball.net/2010/07/improved_regex_for_matching_urls
           reg = new RegExp(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`\!()\[\]{};:'"\.,<>\?]))/g);
         
         p.innerHTML = text.replace(reg, '<a href="$1">$1</a>', 'g')
